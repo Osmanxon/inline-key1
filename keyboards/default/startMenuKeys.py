2bd8e634023adeed753431e24976d4dd43767e9a
Osmanxon <usmonabdullayev0303@gmail.com>
2024-07-31 15:41:20 +0500

    add

---
+++ keyboards/default/startMenuKeys.py
@@ -0,0 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+menuStart=ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='🛍️ Mahsulotlar'),
+            KeyboardButton(text="ℹ️ Qo'llanma")
+        ],
+    ],
+    resize_keyboard=True
+)
